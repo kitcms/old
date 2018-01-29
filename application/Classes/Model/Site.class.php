6abab9df52d01fd745b393b58139a5ba3d1f1f97
Anton Popov <a.popov@kit.team>
2018-01-29 17:49:49 +0500

    Реализация добалвения новых сайтов

--- application/Classes/Model/Site.class.php
+++ application/Classes/Model/Site.class.php
@@ -65,4 +65,56 @@ class Site extends Model
     {
         return $this->hasMany(__NAMESPACE__ . NS .'Section', 'site');
     }
+
+    public function save()
+    {
+        $hosts = array($this->get('host'));
+        if ($alias = $this->isDirty('alias')) {
+            $hosts = array_merge($hosts, (array) $this->get('alias'));
+        }
+        $factory = self::whereHostIn($hosts);
+        if (false === $this->isNew()) {
+            $factory->whereNotIn('id', (array) $this->get('id'));
+        }
+        if ($factory->findOne()) {
+            return false;
+        }
+        return parent::save();
+    }
+
+    public function delete()
+    {
+        // Массивы зависимых объектов
+        $dependences = array();
+        array_push($dependences, $this->section()->findMany());
+        if (parent::delete()) {
+            // Удаление зависимых объектов
+            foreach ((array) $dependences as $dependence) {
+                foreach ((array) $dependence as $depend) {
+                    $depend->delete();
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static function whereHostIn($hosts)
+    {
+        if (function_exists('idn_to_utf8')) {
+            foreach ($hosts as $host) {
+                $hosts[] = idn_to_utf8($host);
+            }
+        }
+        $hosts = array_unique($hosts);
+        $hosts = array_diff($hosts, array(''));
+        $regex = '('. join('|', $hosts) .')';
+        return self::whereAnyIs(
+            array(
+                array('Site.host' => '^'. $regex. '$'),
+                array('Site.alias' => '[[:<:]]'. $regex .'[[:>:]]')
+            ),
+            array('Site.host' => 'REGEXP', 'Site.alias' => 'REGEXP')
+        );
+    }
 }
