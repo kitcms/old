65095fc24b5e6b06d10d20c3225c531136dcd58e
Anton Popov <a.popov@kit.team>
2018-02-03 10:40:07 +0500

    Управление пользователями

--- application/Classes/Model/User.class.php
+++ application/Classes/Model/User.class.php
@@ -59,4 +59,37 @@ class User extends Model
     {
         return $this->hasOne(__NAMESPACE__ . NS .'Group', 'id', 'group');
     }
+
+    public function save()
+    {
+        // Проверка логина на корректность
+        if (!preg_match("/^[[:alnum:]-_.]+$/iu", $this->login) || preg_match("/^[\d-_.]+$/", $this->login)) {
+            return false;
+        }
+        // Проверка уникальности логина и email
+        $clause = '(`login` LIKE ? OR `email` LIKE ?)';
+        $parameters = array($this->get('login'), $this->get('email'));
+        $instance = $this->factory(__CLASS__)->whereRaw($clause, $parameters);
+        if (false === $this->isNew()) {
+            $instance->whereNotIn('id', (array) $this->get('id'));
+        }
+        if ($instance->findOne()) {
+            return false;
+        }
+        // Шифрование пароля
+        if ($this->isDirty('password') && ($password = $this->get('password'))) {
+            $this->set('password', password_hash($password, PASSWORD_DEFAULT));
+        } else {
+            $this->set('password', $this->password);
+        }
+        return parent::save();
+    }
+
+    public function _create_model_instance($orm) {
+        $data = $this->asArray();
+        if (isset($data['password'])) {
+            $this->password = $data['password'];
+        }
+        $this->orm->hydrate($data);
+    }
 }
