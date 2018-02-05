7b3f0cbe2873043680766fbabeffefda7721b561
Anton Popov <a.popov@kit.team>
2018-02-05 13:28:21 +0500

    Набросок функциолана для создания таблиц и добавления новых полей

--- application/Classes/Database/Schema.class.php
+++ application/Classes/Database/Schema.class.php
@@ -16,6 +16,7 @@ use PDO;
 class Schema extends ORM
 {
     protected $_instance_id_column = 'name';
+    protected $_class_name = __NAMESPACE__ .'\Model';
 
     protected $aspects = array(
         'integer' => 'int(11)',
@@ -48,6 +49,82 @@ class Schema extends ORM
         $instance->_where_conditions = array();
         $instance->useIdColumn('field');
         return $instance;
+    }
+
+    protected function _build_insert()
+    {
+        if ('name' === $this->_getIdColumnName()) {
+            $model = new $this->_class_name();
+            foreach ($model->fields() as $field) {
+                $fragments[] = $this->field()->create($field)->_build_field();
+            }
+            $fragments = array(
+                'CREATE TABLE IF NOT EXISTS '. $this->_quoteIdentifier($this->name),
+                '('. $this->_joinIfNotEmpty(", ", $fragments) .')',
+                'ENGINE '. $this->_quoteIdentifier($this->engine),
+                'DEFAULT CHARACTER SET '. $this->_quoteIdentifier(current(explode('_', $this->collation))),
+                'COLLATE '. $this->_quoteIdentifier($this->collation),
+                'COMMENT \''. $this->comment .'\'',
+                'AUTO_INCREMENT 1'
+            );
+            return $this->_joinIfNotEmpty(" ", $fragments);
+        }
+        $fragments = array(
+            'ALTER TABLE',
+            $this->_quoteIdentifier($this->_table_name),
+            'ADD',
+            $this->_build_update_field()
+        );
+        return $this->_joinIfNotEmpty(" ", $fragments);
+    }
+
+    protected function _build_field()
+    {
+        $fragments = array(
+            $this->_quoteIdentifier($this->get('field')),
+            $this->get('type'), // strtoupper($this->get('type'))
+            (filter_var($this->get('null'), FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) ? null : 'NOT NULL'),
+            (strlen($this->get('default')) ? 'default '. self::getDb()->quote($this->get('default')) : null),
+            strtoupper($this->get('extra')),
+            'COMMENT '. self::getDb()->quote($this->get('comment')), // FIXME
+            ($this->get('after') ? 'after '. $this->_quoteIdentifier($this->get('after')) : null),
+            ($this->get('first') ? 'first' : null)
+        );
+        if ($key = strtoupper($this->get('key'))) {
+            if (in_array($key, array('PRI', 'PRIMARY'))) {
+                $key = 'PRIMARY KEY ('. $this->_quoteIdentifier($this->get('field')) .')';
+            } elseif (in_array($key, array('UNI', 'UNIQUE'))) {
+                $key = 'UNIQUE KEY ('. $this->_quoteIdentifier($this->get('field')) .')';
+            } else {
+                $key = 'KEY ('. $this->_quoteIdentifier($this->get('field')) .')';
+            }
+        }
+        return $this->_joinIfNotEmpty(', ', array($this->_joinIfNotEmpty(' ', $fragments), $key));
+    }
+
+    protected function _build_update_field()
+    {
+        $fragments = array(
+            $this->_quoteIdentifier($this->get('field')),
+            $this->get('type'), // strtoupper($this->get('type'))
+            (filter_var($this->get('null'), FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) ? null : 'NOT NULL'),
+            (strlen($this->get('default')) ? 'default '. self::getDb()->quote($this->get('default')) : null),
+            strtoupper($this->get('extra')),
+            'COMMENT '. self::getDb()->quote($this->get('comment')), // FIXME
+            ($this->get('after') ? 'after '. $this->_quoteIdentifier($this->get('after')) : null),
+            ($this->get('first') ? 'first' : null)
+        );
+        /*if ($key = strtoupper($this->get('key'))) {
+            if (in_array($key, array('PRI', 'PRIMARY'))) {
+                $key = 'ADD PRIMARY KEY ('. $this->_quoteIdentifier($this->get('field')) .')';
+            } elseif (in_array($key, array('UNI', 'UNIQUE'))) {
+                $key = 'ADD UNIQUE KEY ('. $this->_quoteIdentifier($this->get('field')) .')';
+            } else {
+                $key = 'ADD KEY ('. $this->_quoteIdentifier($this->get('field')) .')';
+            }
+        }*/
+        $key = array();
+        return $this->_joinIfNotEmpty(', ', array($this->_joinIfNotEmpty(' ', $fragments), $key));
     }
 
     protected function _build_select()
